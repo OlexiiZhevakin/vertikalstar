249138615236766aca1013d04d94a27da493db12
OlexiiZhevakin <o.zhevakin.fronenddev@gmail.com>
2023-07-11 07:38:58 +0300

    fix bugs

--- app/technologies/torkretuvannya/page.tsx
+++ app/technologies/torkretuvannya/page.tsx
@@ -1,7 +1,7 @@
 import Form from "@/components/form/Form"
 import Head from "./section/head/Head"
 import Picture from "./section/picture/Picture"
-import SkleyuvannyTtrishchyn from "./section/skleyuvannya-trishchyn/Skleyuvannya-trishchyn"
+
 
 const Torkretuvannya = () => {
   return(
--- app/technologies/torkretuvannya/section/picture/Picture.tsx
+++ app/technologies/torkretuvannya/section/picture/Picture.tsx
@@ -4,10 +4,10 @@ const Picture = () => {
   return(
     <section className={styles.picture}>
       <div className="container">
-        <h2>Знайома картина?</h2>
+        {/* <h2>Знайома картина?</h2> */}
 
         <div className={styles.inner}>
-          
+          <video src="/img/technologies/page/torkretuvannya/torkretuvannya.mp4" muted autoPlay width={1200}></video>
         </div>
       </div>
     </section>
