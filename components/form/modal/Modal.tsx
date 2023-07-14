84b97cf900d5d2f6eeb5b82a46fbf249b3ba8b5c
OlexiiZhevakin <o.zhevakin.fronenddev@gmail.com>
2023-07-14 13:30:39 +0300

    added article

--- components/form/modal/Modal.tsx
+++ components/form/modal/Modal.tsx
@@ -1,26 +1,46 @@
-
-import styles from "./Modal.module.scss";
+'use client'
+import { useState } from 'react';
+import styles from './Modal.module.scss';
 
 const Modal = () => {
+  const [status, setStatus] = useState('');
 
-  return(
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    const form = event.target;
+    const data = new FormData(form);
+    const response = await fetch(form.action, {
+      method: form.method,
+      body: data,
+    });
+    if (response.ok) {
+      setStatus('Дякую за вашу заявку!');
+      form.reset();
+    } else {
+      setStatus('На жаль, сталася помилка під час відправлення форми.');
+    }
+  };
+
+  return (
     <div className={styles.wrapper}>
       <div>Вкажіть ваші контакти</div>
-        <span>і наш Технолог зв'яжеться з вами в найближчий час</span>
-      <form action="https://de.vertikalstar.com/send-mail.php" method="POST">
+      <span>і наш Технолог зв'яжеться з вами в найближчий час</span>
+      <form
+        action="https://de.vertikalstar.com/send-mail.php"
+        method="POST"
+        onSubmit={handleSubmit}
+      >
+        <label htmlFor="name">Ваше ім'я</label>
+        <input type="text" name="name" id="name" required />
 
-              <label htmlFor="name">Ваше ім'я</label>
-              <input type="text" name="name" id="name" required/>
+        <label htmlFor="tel">Телефон</label>
+        <input type="tel" name="tel" id="tel" required />
 
-              <label htmlFor="tel">Телефон</label>
-              <input type="tel" name="tel" id="tel" required/>
-
-              <button type="submit">
-                Залишити заявку
-              </button>
-            </form>
+        <button type="submit">Залишити заявку</button>
+      </form>
+      <div>{status}</div>
     </div>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
