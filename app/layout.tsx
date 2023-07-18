19362246dedf3ef8914d915a191988b7ae2b6ddc
OlexiiZhevakin <o.zhevakin.fronenddev@gmail.com>
2023-07-18 14:57:11 +0300

    add binotel

--- app/layout.tsx
+++ app/layout.tsx
@@ -88,9 +88,9 @@ export default function RootLayout({
         <Script>
           {`
             (function(d, w, s) {
-	            var widgetHash = 'BH3A4rHyNlvH', gcw = d.createElement(s); gcw.type = 'text/javascript'; gcw.async = true;
-	            gcw.src = '//widgets.binotel.com/getcall/widgets/'+ widgetHash +'.js';
-	            var sn = d.getElementsByTagName(s)[0]; sn.parentNode.insertBefore(gcw, sn);
+              var widgetHash = 'BH3A4rHyNlvH', gcw = d.createElement(s); gcw.type = 'text/javascript'; gcw.async = true;
+              gcw.src = '//widgets.binotel.com/getcall/widgets/'+ widgetHash +'.js';
+              var sn = d.getElementsByTagName(s)[0]; sn.parentNode.insertBefore(gcw, sn);
             })(document, window, 'script');
           `}
         </Script>
