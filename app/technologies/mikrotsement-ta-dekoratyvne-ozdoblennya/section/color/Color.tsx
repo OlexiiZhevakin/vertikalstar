c6cb55619b95f7d98b49df1038075cf23ae4819f
OlexiiZhevakin <o.zhevakin.fronenddev@gmail.com>
2023-07-24 19:20:05 +0300

    add leeloo

--- app/technologies/mikrotsement-ta-dekoratyvne-ozdoblennya/section/color/Color.tsx
+++ app/technologies/mikrotsement-ta-dekoratyvne-ozdoblennya/section/color/Color.tsx
@@ -8,7 +8,8 @@ const Color = () => {
     <section className={styles.container}>
       <div className="container">
         <h2 className={styles.title}>96 відтінків кольорів</h2>
-        <Image className={styles.img} src='/img/technologies/page/microcement/color/96-vidtinkiv-koloriv.jpg' alt='96 відтінків кольорів' title='96 відтінків кольорів' width={400} height={400}/>
+        <Image className={styles.img} src='/img/technologies/page/microcement/color/96-vidtinkiv-koloriv-1.jpg' alt='96 відтінків кольорів' title='96 відтінків кольорів' width={400} height={400}/>
+        <Image className={styles.img} src='/img/technologies/page/microcement/color/96-vidtinkiv-koloriv-2.jpg' alt='96 відтінків кольорів' title='96 відтінків кольорів' width={400} height={558}/>
       </div>
     </section>
   )
