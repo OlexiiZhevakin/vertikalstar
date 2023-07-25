215a8571a4070fe41b0d3ad28689085e4c3bbaf0
OlexiiZhevakin <o.zhevakin.fronenddev@gmail.com>
2023-07-25 08:16:01 +0300

    add leeloo

--- app/technologies/mikrotsement-ta-dekoratyvne-ozdoblennya/page.tsx
+++ app/technologies/mikrotsement-ta-dekoratyvne-ozdoblennya/page.tsx
@@ -1,4 +1,5 @@
 
+
 // import Script from 'next/script'
 // import Advantage from './section/advantage/Advantage'
 // import Color from './section/color/Color'
@@ -8,32 +9,31 @@
 // import Pryklady from './section/pryklady/Pryklady'
 // import Technologiya from './section/technologiya/Technologiya'
 // import Video from './section/video/Video'
-// import { FC } from 'react'
 
-
-// const Microcement: FC = () => {
-  
+// const Microcement: React.FC = () => {
 //   return (
 //     <>
-//       <HeaderPage/>
-//       <Pryklady/>
-//       <Video/>
-//       <Technologiya/>
-//       <Advantage/>
-//       <Color/>
-//       <Materialy/>
-//       <Help/>
-//       <Script strategy="afterInteractive">
-//         {`window.LEELOO_LEADGENTOOLS = (window.LEELOO_LEADGENTOOLS || []).concat('mu0um2');`}
-//       </Script>
+//       <HeaderPage />
+//       <Pryklady />
+//       <Video />
+//       <Technologiya />
+//       <Advantage />
+//       <Color />
+//       <Materialy />
+//       <Help />
+//       <Script strategy="afterInteractive" dangerouslySetInnerHTML={{
+//         __html: `
+//           window.LEELOO_LEADGENTOOLS = (window.LEELOO_LEADGENTOOLS || []).concat('mu0um2');
+//         `
+//       }} />
 //     </>
 //   )
 // }
 
 // export default Microcement
 
-
-import Script from 'next/script'
+'use client'
+import { useEffect } from 'react'
 import Advantage from './section/advantage/Advantage'
 import Color from './section/color/Color'
 import HeaderPage from './section/header/Head'
@@ -44,6 +44,18 @@ import Technologiya from './section/technologiya/Technologiya'
 import Video from './section/video/Video'
 
 const Microcement: React.FC = () => {
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.innerHTML = `
+      window.LEELOO_LEADGENTOOLS = (window.LEELOO_LEADGENTOOLS || []).concat('mu0um2');
+    `
+    document.body.appendChild(script)
+
+    return () => {
+      document.body.removeChild(script)
+    }
+  }, [])
+
   return (
     <>
       <HeaderPage />
@@ -54,13 +66,9 @@ const Microcement: React.FC = () => {
       <Color />
       <Materialy />
       <Help />
-      <Script strategy="afterInteractive" dangerouslySetInnerHTML={{
-        __html: `
-          window.LEELOO_LEADGENTOOLS = (window.LEELOO_LEADGENTOOLS || []).concat('mu0um2');
-        `
-      }} />
     </>
   )
 }
 
 export default Microcement
+
