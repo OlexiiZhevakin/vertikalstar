ace25c955f7079e0d8bd7bc1480135a21ba3ea97
OlexiiZhevakin <o.zhevakin.fronenddev@gmail.com>
2023-07-27 10:02:06 +0300

    add new obj

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,52 +1,13 @@
 
-// import Header from '@/components/header/Header'
-// import './globals.scss'
-// import { Inter, Roboto } from 'next/font/google'
-// import Footer from '@/components/footer/Footer'
-// import { Metadata } from 'next'
-// import SocialMediaBlock from '@/components/social/SocialMediaBlock'
-// // import { CookiesProvider } from 'react-cookie';
-
-// const inter = Inter({ subsets: ['latin'] })
-
-
-// export const metadata: Metadata = {
-//   metadataBase: new URL('https://www.vertikalstar.com/'),
-//   title: 'Вертикаль Будівельних Технологій',
-//   description: 'Вертикаль Будівельних Технологій - лідер на ринку будівельних послуг, який спеціалізується на наданні інноваційних рішень в галузі будівництва та ремонту будівель.',
-//   openGraph: {
-//     images: ['/img/head-bg.webp']
-//   }
-// };
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode
-// }) {
-//   return (
-//     <html lang="uk-UA">
-//       <body className={inter.className}>
-
-//           <Header />
-//           <SocialMediaBlock />
-//           <main>
-//             {children}
-//           </main>
-//           <Footer />
-
-//       </body>
-//     </html>
-//   )
-// }
-
-
 import Script from 'next/script'
 import Header from '@/components/header/Header'
 import './globals.scss'
-import { Inter, Roboto } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import Footer from '@/components/footer/Footer'
 import { Metadata } from 'next'
 import SocialMediaBlock from '@/components/social/SocialMediaBlock'
+
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -65,21 +26,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+
     <html lang="uk-UA">
       <body className={inter.className}>
-        {/* <Script>
-          {`
-            window.LEELOO = function(){
-              window.LEELOO_INIT = { id: '641c65871a6a5aa739dbce48' };
-              var js = document.createElement('script');
-              js.src = 'https://app.leeloo.ai/init.js';
-              js.async = true;
-              document.getElementsByTagName('head')[0].appendChild(js);
-            }; 
-            LEELOO();
-          `}
-        </Script> */}
-        
+        <Script 
+          id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="eb4a56fa-dcf5-4ac0-bf2b-22e8c0bb2b37" data-blockingmode="auto" type="text/javascript">
+        </Script>
+        <Script id="CookieDeclaration" src="https://consent.cookiebot.com/eb4a56fa-dcf5-4ac0-bf2b-22e8c0bb2b37/cd.js" type="text/javascript" async>
+        </Script>
         <Script src="https://www.googletagmanager.com/gtag/js?id=G-NSG4YJR32Z" />
         <Script id="google-analytics">
           {`
@@ -110,11 +64,11 @@ export default function RootLayout({
         <Header />
         <SocialMediaBlock />
         <main>
-          
           {children}
         </main>
         <Footer />
       </body>
     </html>
+
   )
 }
