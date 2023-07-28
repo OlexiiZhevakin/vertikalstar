4b410942f69b163dde523846e8263790e21e7b5e
OlexiiZhevakin <o.zhevakin.fronenddev@gmail.com>
2023-07-28 22:03:45 +0300

    add new photo services

--- app/technologies/mikrotsement-ta-dekoratyvne-ozdoblennya/section/leeloo/Leeloo.tsx
+++ app/technologies/mikrotsement-ta-dekoratyvne-ozdoblennya/section/leeloo/Leeloo.tsx
@@ -8,7 +8,7 @@ const Leeloo = () => {
         <div className={styles.block}>
           <Image className={styles.img} src='/img/technologies/page/microcement/microcement.jpg' alt='Ми приготували для вас збірник прикладів застосування мікроцементу та покрокову технологію його нанесення' title='Ми приготували для вас збірник прикладів застосування мікроцементу та покрокову технологію його нанесення' width={500} height={500} />
           <div className={styles.content}>
-            <p className={styles.text}>Ми приготували для вас збірник прикладів застосування мікроцементу та покрокову технологію його нанесення
+            <p className={styles.text}>Ми підготували для вас збірник прикладів застосування мікроцементу та покрокову технологію його нанесення
             </p>
             <div className={styles.btn}>
               <Image src='/img/technologies/page/microcement/file-pdf-white.svg' alt='Іконка PDF' title='Іконка PDF' width={30} height={30}/>
