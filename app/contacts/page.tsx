aaa6d91d797d944f29529472879cf31a3a81241a
OlexiiZhevakin <o.zhevakin.fronenddev@gmail.com>
2023-08-03 17:35:56 +0300

    fix more page

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,7 +1,16 @@
 import Form from "@/components/form/Form";
 import Head from "./section/head/Head";
 import Info from "./section/info/Info";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  metadataBase: new URL('https://www.vertikalstar.com/contacts'),
+  title: 'Контакти',
+  description: 'Контакти компанії Вертикаль Будівельних Технологій',
+  openGraph: {
+    images: ['/img/contacts/contact-bg.webp']
+  }
+}
 
 const ContactsPage = () => {
   return (
