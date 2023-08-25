9cded3153430720b0ef70c112a9b8dda39a6674e
OlexiiZhevakin <o.zhevakin.fronenddev@gmail.com>
2023-08-25 17:03:11 +0300

    fix

--- app/[locale]/components/swich-lang/SwichLang.tsx
+++ app/[locale]/components/swich-lang/SwichLang.tsx
@@ -1,21 +1,24 @@
+
 import Link from 'next-intl/link';
 import styles from './swich.module.scss'
-import { usePathname } from 'next/navigation'
+
+
+
 
 const SwichLang = () => {
-  const pathname = usePathname()
+
   return (
     <div className={styles.links}>
-      <Link className={styles.link} href={pathname} locale='uk'>
+      <Link className={styles.link} href='/' locale='uk'>
         ua
       </Link>
-      <Link className={styles.link} href={pathname} locale='en'>
+      <Link className={styles.link} href='/' locale='en'>
         en
       </Link>
-      <Link className={styles.link} href={pathname} locale='de'>
+      <Link className={styles.link} href='/' locale='de'>
         de
       </Link>
-      <Link className={styles.link} href={pathname} locale='ru'>
+      <Link className={styles.link} href='/' locale='ru'>
         ru
       </Link>
     </div>
