d08b994dda07cdeb90394bffecee32c8ab8978a3
OlexiiZhevakin <o.zhevakin.fronenddev@gmail.com>
2023-09-08 09:26:35 +0300

    add lang

--- app/[locale]/components/swich-lang/SwichLang.tsx
+++ app/[locale]/components/swich-lang/SwichLang.tsx
@@ -1,26 +1,58 @@
 'use client'
-import Link from 'next-intl/link';
-import styles from './swich.module.scss'
+// import Link from 'next-intl/link';
+// import styles from './swich.module.scss'
 
+
+// const SwichLang = () => {
+
+  
+//   return (
+//     <div className={styles.links}>
+//       <Link className={styles.link} href='/' locale='uk'>
+//         ua
+//       </Link>
+//       <Link className={styles.link} href='/' locale='en'>
+//         en
+//       </Link>
+//       <Link className={styles.link} href='/' locale='de'>
+//         de
+//       </Link>
+//       <Link className={styles.link} href='/' locale='ru'>
+//         ru
+//       </Link>
+//     </div>
+//   )
+// }
+
+// export default SwichLang
+import { usePathname, useRouter } from 'next-intl/client';
+import Link from 'next-intl/link';
+import styles from './swich.module.scss';
 
 const SwichLang = () => {
-  
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const handleLocaleChange = (locale: string) => {
+    router.replace(pathname, { locale });
+  };
+
   return (
     <div className={styles.links}>
-      <Link className={styles.link} href='/' locale='uk'>
+      <Link className={styles.link} href={pathname} locale="uk" onClick={() => handleLocaleChange('uk')}>
         ua
       </Link>
-      <Link className={styles.link} href='/' locale='en'>
+      <Link className={styles.link} href={pathname} locale="en" onClick={() => handleLocaleChange('en')}>
         en
       </Link>
-      <Link className={styles.link} href='/' locale='de'>
+      <Link className={styles.link} href={pathname} locale="de" onClick={() => handleLocaleChange('de')}>
         de
       </Link>
-      <Link className={styles.link} href='/' locale='ru'>
+      <Link className={styles.link} href={pathname} locale="ru" onClick={() => handleLocaleChange('ru')}>
         ru
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default SwichLang
+export default SwichLang;
